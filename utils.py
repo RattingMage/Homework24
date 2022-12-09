a18b37b0b443d730e922461307bdfddcbd964c32
RattingMage <66022822+RattingMage@users.noreply.github.com>
2022-12-09 18:59:00 +0300

    Fix filter and unique

--- utils.py
+++ utils.py
@@ -16,15 +16,16 @@ def map_query(param: str, data: list[str]) -> list[str]:
 
 
 def unique_query(data: list[str], *args, **kwargs) -> list[str]:
-    result: list = []
-    seen: set = set()
-    for row in data:
-        if row in seen:
-            continue
-        else:
-            result.append(row)
-            seen.add(row)
-    return result
+    # result: list = []
+    # seen: set = set()
+    # for row in data:
+    #     if row in seen:
+    #         continue
+    #     else:
+    #         result.append(row)
+    #         seen.add(row)
+    # return result
+    return list(set(data))
 
 
 def sort_query(param: str, data: list[str]) -> list[str]:
@@ -43,7 +44,7 @@ def regex_query(param: str, data: list[str]) -> list[str]:
 
 
 CMD_TO_FUNCTION = {
-    "fileter": filter_query,
+    "filter": filter_query,
     "map": map_query,
     "unique": unique_query,
     "sort": sort_query,
